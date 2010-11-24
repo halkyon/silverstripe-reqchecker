67a2e2048bf54b1d142fef274da6350d22b96372
unknown <sharvey@.(none)>
2010-11-24 20:49:47 +1300

    Add GD version at the end of successful assertions, split the extension check into another method on RequirementsChecker

--- silverstripe_requirements.php
+++ silverstripe_requirements.php
@@ -91,15 +91,9 @@ class RequirementsChecker {
 		return version_compare($version, PHP_VERSION, '<=');
 	}
 
-	/**
-	 * Check a loaded PHP extension is of at least the version specified.
-	 * @param string $name Name of extension to check, e.g. "gd"
-	 * @param mixed $version Minimum version of extension to assert
-	 * @return boolean TRUE passed assertion | FALSE failed assertion
-	 */
-	public function assertMinimumPhpExtensionVersion($name, $version) {
+	public function getPhpExtensionVersion($name) {
 		$info = $this->getPhpInfo();
-		$foundvers = null;
+		$version = null;
 
 		if(isset($info[$name])) {
 			// find a key with "version" text and get that value
@@ -113,12 +107,22 @@ class RequirementsChecker {
 
 			if(isset($info[$name][$found])) {
 				preg_match('/\d+\.\d+(?:\.\d+)?/', $info[$name][$found], $matches);
-				if(isset($matches[0])) $foundvers = $matches[0];
-				elseif(is_numeric($info[$name][$found])) $foundvers = $info[$name][$found];
+				if(isset($matches[0])) $version = $matches[0];
+				elseif(is_numeric($info[$name][$found])) $version = $info[$name][$found];
 			}
 		}
 
-		return version_compare($version, $foundvers, '<=');
+		return $version;
+	}
+
+	/**
+	 * Check a loaded PHP extension is of at least the version specified.
+	 * @param string $name Name of extension to check, e.g. "gd"
+	 * @param mixed $version Minimum version of extension to assert
+	 * @return boolean TRUE passed assertion | FALSE failed assertion
+	 */
+	public function assertMinimumPhpExtensionVersion($name, $version) {
+		return version_compare($version, $this->getPhpExtensionVersion($name, $version), '<=');
 	}
 
 }
@@ -137,15 +141,13 @@ class RequirementsFormatter {
 		}
 	}
 
-	public function showAssertion($name, $message = '', $tag = 'span') {
-		$result = '';
-		if(is_bool($message) && $message == true) {
-			$status = 'pass';
-			$result = 'PASSED: ' . $name;
+	public function showAssertion($name, $result, $message = '', $tag = 'span') {
+		if($result == true) {
+			$status = 'passed';
 		} else {
-			$status = 'fail';
-			$result = 'FAILED: ' . $name . ($message ? sprintf(' (%s)', $message) : '');
+			$status = 'failed';
 		}
+		$result = strtoupper($status) . ': ' . $name . ($message ? sprintf(' (%s)', $message) : '');
 		return $this->show(($tag ? sprintf('<%s class="%s">', $tag, $status) : '') . $result . ($tag ? sprintf('</%s>', $tag) : ''));
 	}
 
@@ -187,16 +189,15 @@ echo $f->heading('System information', 2);
 echo $f->show(sprintf('System: %s', isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'Unknown'));
 echo $f->show(sprintf('Server Software: %s', isset($_SERVER['SERVER_SOFTWARE']) ? $_SERVER['SERVER_SOFTWARE'] : 'Unknown'));
 echo $f->show(sprintf('Server API: %s', php_sapi_name()));
+echo $f->show(sprintf('PHP Version: %s', PHP_VERSION));
+echo $f->show(sprintf('PHP configuration file path: %s', get_cfg_var('cfg_file_path')));
 echo $f->nl();
 
 echo $f->heading('PHP configuration', 2);
-echo $f->show(sprintf('Version: %s', PHP_VERSION));
-echo $f->show(sprintf('php.ini path: %s', get_cfg_var('cfg_file_path')));
-echo $f->nl();
 
 echo $f->heading('PHP version', 3);
-echo $f->showAssertion('PHP version at least 5.2.0', $req->assertMinimumPhpVersion('5.2.0'));
-echo $f->show('');
+echo $f->showAssertion('PHP version at least 5.2.0', $req->assertMinimumPhpVersion('5.2.0'), PHP_VERSION);
+echo $f->nl();
 
 echo $f->heading('PHP configuration options', 3);
 echo $f->showAssertion('asp_tags option set to Off', $req->assertPhpIniOptionOff('asp_tags'));
@@ -212,7 +213,7 @@ echo $f->heading('PHP extensions', 3);
 echo $f->showAssertion('curl extension loaded', extension_loaded('curl'));
 echo $f->showAssertion('dom extension loaded', extension_loaded('dom'));
 echo $f->showAssertion('gd extension loaded', extension_loaded('gd'));
-echo $f->showAssertion('gd extension is at least version 2.0', $req->assertMinimumPhpExtensionVersion('gd', '2.0'));
+echo $f->showAssertion('gd extension is at least version 2.0', $req->assertMinimumPhpExtensionVersion('gd', '2.0'), $req->getPhpExtensionVersion('gd'));
 echo $f->showAssertion('iconv extension loaded', extension_loaded('iconv'));
 echo $f->showAssertion('mbstring extension loaded', extension_loaded('mbstring'));
 if(!preg_match('/WIN/', PHP_OS)) echo $f->showAssertion('posix extension loaded', extension_loaded('posix'));
